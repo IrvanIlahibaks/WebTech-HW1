03ae5d7b739cc7bd7f4290fb61ab05abc35a04bf
Ljvdv <Delalcon@gmail.com>
2024-03-13 18:50:21 +0100

    nav bar

--- ResponsiveNav.js
+++ ResponsiveNav.js
@@ -1,11 +1,11 @@
 /* from https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 Change css from responsive and back by clicking the hamburger button.
 */
-function myFunction() {
-    var navtest = document.getElementById("myNavigatie");
-    if (navtest.className === "navigatie") {
-      navtest.className += " responsive";
-    } else {
-      navtest.className = "navigatie";
-    }
+function myResponsive() {
+  var zz = document.getElementById("myNavigatie");
+  if (zz.className === "navigatie") {
+    zz.className += " responsive";
+  } else {
+    zz.className = "navigatie";
   }
+}
