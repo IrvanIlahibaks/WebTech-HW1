8443eab2e733c8b5a2e8b9f08b7f8e2a790ab5ae
Quintijn Eijkenboom <q.h.w.eijkenboom@students.uu.nl>
2024-03-15 23:15:52 +0100

    A lot of small changes

--- info.js
+++ info.js
@@ -1,3 +1,4 @@
+// Initialize classes
 class CreativeWork {
     constructor(title, year, authors) {
         this.title = title;
@@ -49,7 +50,6 @@ class Publisher extends Company {
 //Change the appearance of elements on the page.
 var selectedElement = 'body, p';
 
-
 function selectElement(value) {
     selectedElement = value; 
 }
@@ -67,35 +67,37 @@ function changeAppearance(value) {
             case "lFontWhiteBg":
                 element.style.fontSize = "large";
                 element.style.backgroundColor = "white";
+                element.style.color = "black";
                 break;
             case "mFontBlackBg":
                 element.style.fontSize = "medium";
                 element.style.backgroundColor = "black";
+                element.style.color = "white";
                 break;
             case "mFontWhiteBg":
                 element.style.fontSize = "medium";
                 element.style.backgroundColor = "white";
+                element.style.color = "black";
                 break;
             case "sFontBlackBg":
                 element.style.fontSize = "small";
                 element.style.backgroundColor = "black";
+                element.style.color = "white";
                 break;
             // Add more cases as necessary
         }
     });
 }
 
-
-
-// Eerst author maken
+// Make an author
 const Chugong = new Author ("Chungong", "unkown birthday",
 ["Solo Leveling, vol.1", "Solo Leveling, vol.2"], "https://en.wikipedia.org/wiki/Solo_Leveling")
 
-//Publisher maken
+// Make a publlisher
 const KakaoPage = new Publisher("KakaoPage", "https://en.wikipedia.org/wiki/KakaoPage", 
 ["Title 1", "Title2"])
 
-//Book instance
+// make a book instance
 const Sololevelingvol1 = new Book ("Solo Leveling vol.1", 2016, Chugong, "Fantasy", KakaoPage, 
 "https://www.worldswithoutend.com/covers/CG_solole01.jpg", "In a world where hunters — human warriors who possess supernatural abilities — must battle deadly monsters to protect mankind from certain annihilation, a notoriously weak hunter named Sung Jinwoo finds himself in a seemingly endless struggle for survival. One day, after narrowly surviving an overwhelmingly powerful double dungeon that nearly wipes out his entire party, a mysterious program called the System chooses him as its sole player and in turn, gives him the unique ability to level up in strength. This is something no other hunter is able to do, as a hunter's abilities are set once they awaken. He also unlocks a deeper ability to turn anyone he kills into a loyal minion called a Shadow. Jinwoo then sets out on a journey as he fights against all kinds of enemies, both man and monster, to discover the secrets of the dungeons and the true source of his powers.")
 
@@ -104,8 +106,8 @@ const sectionInfo = document.createElement("section"); //make section
 
 const headerOne = document.createElement("h1"); //make h1
 const headerOneText = document.createTextNode("Title: " + Sololevelingvol1.title);
-headerOne.appendChild(headerOneText); //make headerOneText into child of headerOne
-sectionInfo.appendChild(headerOne);        //make headerOne into child of sectionInfo
+headerOne.appendChild(headerOneText); // Make headerOneText into child of headerOne
+sectionInfo.appendChild(headerOne); // Make headerOne into child of sectionInfo
 
 const pGenre = document.createElement("p");
 const pGenretext = document.createTextNode("Genre: " + Sololevelingvol1.genre);
@@ -139,16 +141,3 @@ sectionInfo.appendChild(pPlot);
 
 
 document.body.appendChild(sectionInfo);
-
-
-
-
-/* Format for new paragraph.
-
-const pOne = document.createElement("p");
-const pOnetext = document.createTextNode("Paragraph 1");
-pOne.appendChild(pOnetext);
-sectionInfo.appendChild(pOne); */
-
-
-
