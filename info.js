fe5d6ff867e37407860c54e195f169d3ec5176d1
Ljvdv <Delalcon@gmail.com>
2024-03-15 20:51:10 +0100

    invullen info pagina

--- info.js
+++ info.js
@@ -46,13 +46,26 @@ class Publisher extends Company {
     }
 }
 
+//Afbeelding 
 
+
+// Eerst author maken
+const Chugong = new Author ("Chungong", "unkown birthday",
+["Solo Leveling, vol.1", "Solo Leveling, vol.2"], "https://en.wikipedia.org/wiki/Solo_Leveling")
+
+//Publisher maken
+const KakaoPage = new Publisher("KakaoPage", "https://en.wikipedia.org/wiki/KakaoPage", 
+["Title 1", "Title2"])
+
+//Book instance
+const Sololevelingvol1 = new Book ("Solo Leveling vol.1", 2016, Chugong, "Fantasy", KakaoPage, 
+"https://www.worldswithoutend.com/covers/CG_solole01.jpg", "Plot van boek")
 
 /* making a DOM tree: pOnetext is a node of pOne, pOne is a node of divFirst etcetera */
 const divFirst = document.createElement("div"); //make div
 
 const headerOne = document.createElement("h1"); //make h1
-const headerOneText = document.createTextNode("Heading");
+const headerOneText = document.createTextNode("Title: " + Sololevelingvol1.title);
 headerOne.appendChild(headerOneText); //make headerOneText into child of headerOne
 divFirst.appendChild(headerOne);        //make headerOne into child of divFirst
 
@@ -65,7 +78,6 @@ divFirst.appendChild(pOne);
 const pTwo = document.createElement("p");
 const pTwotext = document.createTextNode("Paragraph 2");
 pTwo.appendChild(pTwotext);
-
 divFirst.appendChild(pTwo);
 
 
